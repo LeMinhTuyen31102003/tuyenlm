7d5fac8b797a36b99a709cdf3b0bae61ae6c42d0
LeMinhTuyen31102003 <leminhtuyen1111@gmail.com>
2026-01-12 23:00:44 +0700

    feat: init

---
+++ src/main/java/example/ecommerce/tuyenlm/TuyenlmApplication.java
@@ -0,0 +1,13 @@
+package example.ecommerce.tuyenlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuyenlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuyenlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/ecommerce/tuyenlm/TuyenlmApplicationTests.java
@@ -0,0 +1,13 @@
+package example.ecommerce.tuyenlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuyenlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
